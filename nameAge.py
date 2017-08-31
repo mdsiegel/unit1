183cc5301b8b228a1c66db60611b792cf30c878d
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-08-31 14:48:59 -0400

    Updated from Brython Server: 8/31/2017 2:48:54 PM

--- nameAge.py
+++ nameAge.py
@@ -2,9 +2,9 @@
 #8/31/17
 #nameAge.py - Getting info based on input
 
-Name = input('Enter your first and last name ' )
+Name = input('Enter your first and last name. ' )
 FirstName, LastName = Name.split()
-Age = int(input('Enter your age '))
+Age = int(input('Enter your age. '))
 print('Your first name has', len(FirstName), 'letters')
 print('Your last name has', len(LastName), 'letters')
 print('Next year you will be ', Age + 1, 'years old')
