ac77e597df5cb82ecf8a7a248dc94b39a368fd64
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-07 14:26:45 -0400

    Updated from Brython Server: 9/7/2017 2:26:43 PM

--- change.py
+++ change.py
@@ -1,1 +1,8 @@
+#Matthew Siegel
+#9/7/17
+#change.py - getting the amount of change
 
+cents = int(input('Enter the amount of cents you have: '))
+quarters = cents//25
+dimes = (cents-(quarters*25))//10
+nickles = (cents-(quarters*25)-(dimes*10)
