b2c1190719f44995df14c5bc45f756cc340b1e4a
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-07 14:28:28 -0400

    Updated from Brython Server: 9/7/2017 2:28:26 PM

--- change.py
+++ change.py
@@ -5,4 +5,6 @@
 cents = int(input('Enter the amount of cents you have: '))
 quarters = cents//25
 dimes = (cents-(quarters*25))//10
-nickles = (cents-(quarters*25)-(dimes*10)
+nickles = (cents-(quarters*25)-(dimes*10))//5
+pennies = (cents-(quarters*25)-(dimes*10)-(nickles*5))//1
+print('You have',quarters,'quarters',dimes,'dimes',nickles,'nickles',pennies,'pennies')
