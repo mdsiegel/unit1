021429c7d1e43fd053fe7664ae3de79a90c94576
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-08 14:38:41 -0400

    Updated from Brython Server: 9/8/2017 2:38:34 PM

--- binary.py
+++ binary.py
@@ -5,13 +5,14 @@
 number = int(input('Enter an 8 diget binary: '))
 
 one = number%10
-two = one%10
-three = 
-four = (number//1000)%10000
-five = (number//10000)%100000
-six = (number//100000)%1000000
-seven = (number//1000000)%10000000
-eight = (number//10000000)%100000000
+two = (number//10)%10
+three = (number//100)%10
+four = (number//1000)%10
+five = (number//10000)%10
+six = (number//100000)%10
+seven = (number//1000000)%10
+eight = (number//10000000)%10
+print(two)
 
 totalNumber = (one*2**0)+(two*2**1)+(three*2**2)+(four*2**3)+(five*2**4)+(six*2**5)+(seven*2**6)+(eight*2**7)
 print(totalNumber)
