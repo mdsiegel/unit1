49b17fabd9c306e0462318cdb16823d1605d4bcd
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-08 14:28:15 -0400

    Updated from Brython Server: 9/8/2017 2:28:08 PM

--- binary.py
+++ binary.py
@@ -2,5 +2,16 @@
 #9/7/17
 #binary.py - finding binary numbers
 
-binary = int(input('Enter an 8 diget binary: '))
+number = int(input('Enter an 8 diget binary: '))
+
+one = number%10
+two = number%100
+three = number%1000
+four = number%10000
+five = number%100000
+six = number%1000000
+seven = number%10000000
+eight = number%100000000
+
+totalNumber
 
