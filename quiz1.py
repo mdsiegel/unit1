306af30605013594ef1063a9525ff19d1f126553
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-11 14:16:25 -0400

    Updated from Brython Server: 9/11/2017 2:16:24 PM

--- quiz1.py
+++ quiz1.py
@@ -4,4 +4,9 @@
 
 print("Matthew Siegel")
 
-number1 = float(input("Enter a number: "
+number1 = float(input("Enter a number: "))
+number2 = float(input("Enter another number: "))
+
+print("The sum of the numbers is",number1+number2, "and the product of the numbers is",number1*number2)
+print("Your lucky number is",(number1*7)+(number2*7))
+
